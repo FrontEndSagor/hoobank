34fbfd4c2a642467874d23753f87331e5d0b6f5b
Mohammad Sagor Ali <sagorbbr1@gmail.com>
2023-04-23 20:34:58 +0600

    CTA Clients and Testimonials are added

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import {card} from '../assets';
+import styles , {layout} from '../style';
+import Button from './Button';
 
 function CardDeal() {
   return (
-    <div>CardDeal</div>
+   <section className={`${layout.section}`}>
+    <div className={`${layout.sectionInfo}`}>
+      <h2 className={`${styles.heading2}`}>Find a betetr card deal <br className='sm:block hidden'/> in few easy steps</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione architecto, dolores provident optio praesentium adipisci, impedit fugiat magni doloremque consectetur ut similique voluptates voluptate, fuga quae? Aperiam harum natus esse!</p>
+      <Button styles='mt-10' />
+    </div>
+    <div className={`${layout.sectionImg}`}>
+    <img src={card} alt="card" className='w-[100%] h-[100%]' />
+    </div>
+   </section>
   )
 }
 
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
-
+import React from 'react';
+import {stats} from '../constant';
+import styles from '../style';
 function Stats() {
   return (
-    <div>Stats</div>
+    <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
+      {stats.map((item) => {
+        return <div key={item.id} className={`flex-1 flex justify-start items-center flex-row m-3`}>
+          <h4 className='font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] text-white'>
+            {item.value}
+          </h4>
+          <p className='font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px] text-gradient ml-3 uppercase'>{item.title}</p>
+        </div>
+      })}
+    </section>
   )
 }
 
